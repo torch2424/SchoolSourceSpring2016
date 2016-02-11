4712f6be2956130a87d8da0471cab0e52da118f8
Aaron Turner <aaron@aaronthedev.com>
2016-02-10 20:20:59 -0800

    Fixed up the program some
    Made it more readable by placing logic in function, and better error checking,
    simply need to check for /* comments

--- CapitalSource/src/capitalizer.java
+++ CapitalSource/src/capitalizer.java
@@ -72,7 +72,7 @@ public class capitalizer {
 		"struct", "switch", "this", "throw", "true", "try", "typeof",
 		"uint", "ulong", "unchecked", "unsafe", "ushort", "using",
 		"virtual", "void", "volatile", "while", "add", "alias", "ascending",
-		"async", "await", "descending", "dynamic", "from", "get", "global", 
+		"async", "await", "descending", "dynamic", "from", "get", "global",
 		"group", "into", "join", "let", "orderby", "partial", "partial",
 		"remove", "select", "set", "value", "var", "where",
 		"where", "yield"};
@@ -90,136 +90,152 @@ public class capitalizer {
 
 		// Open our file chooser
 		JFileChooser chooser = new JFileChooser();
-		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-		{
-			// Get the file
-			File selectedFile = chooser.getSelectedFile();
-			
-			System.out.println("You selected the file: " + selectedFile.getName());
 
-			//Check if the file is the correct source code
-			if(!selectedFile.getName().endsWith(".java") &&
-					!selectedFile.getName().endsWith(".c") &&
-					!selectedFile.getName().endsWith(".cpp") &&
-					!selectedFile.getName().endsWith(".cs"))
-			{
-				//Wrong file type inform the user
-				System.out.println("Wrong File type! Please reload the application, and enter a .java, .c, .cpp, or .cs file");
-				System.out.println("Thanbk you for using the Capitalizer Application!");
-			}
-			else {
-				
-				//They entered the correct file type
-				//create a map to assign our array to
-				HashMap<String, Integer> reserved = new HashMap<String, Integer>();
-				
-				//Find what file type it was and use the correct array to assign the keys of our map
-				if(selectedFile.getName().endsWith(".java")) reserved = setKeys(reserved, javaWords);
-				else if(selectedFile.getName().endsWith(".c")) reserved = setKeys(reserved, cWords);
-				else if(selectedFile.getName().endsWith(".cpp")) reserved = setKeys(reserved, cppWords);
-				else if(selectedFile.getName().endsWith(".cs")) reserved = setKeys(reserved, csWords);
-				
-				try {
-					
-					//Create a scanner and read in every line and capitalize the reserved words
-					Scanner scan = new Scanner(selectedFile);
-					
-					//Create our FileWriter and print to another file
-					//First create the File, using the directory the user launched the app
-					File outputFile = new File(System.getProperty("user.dir") + "/capitalized.txt");
-					//Delete the file so we can re-write it
-					outputFile.delete();
-					FileWriter writer = new FileWriter(outputFile, true);
-					
-					//loop until no more lines
-					while(scan.hasNextLine()) {
-						
-						//read in the line
-						String newLine = scan.nextLine();
-						
-						//Procedd only if it is not commented
-						if(!newLine.contains("//")) {
-							
-							//find if the line has any reserved words
-							for(String key : reserved.keySet()) {
-									
-									//Count the number of times it occurs
-									Pattern regex = Pattern.compile(key + "[//{//( ]|" + key + "$");
-								    Matcher m = regex.matcher(newLine);
-								    int count = 0;
-								    while (m.find()){
-								    	
-										//capitalize the sub string
-										newLine = newLine.replaceFirst(key, key.toUpperCase());
-										
-								    	count++;
-								    }
-									
-									//Increase the value of the key at that point
-									reserved.put(key, reserved.get(key) + count);
+			//Set a variable for if we chose a valid file
+			boolean validFile = false;
+
+			while(!validFile) {
+
+					if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+					{
+							// Get the file
+							File selectedFile = chooser.getSelectedFile();
+
+							System.out.println("You selected the file: " + selectedFile.getName());
+
+							//Check if the file is the correct source code
+							if(!selectedFile.getName().endsWith(".java") &&
+									!selectedFile.getName().endsWith(".c") &&
+									!selectedFile.getName().endsWith(".cpp") &&
+									!selectedFile.getName().endsWith(".cs"))
+							{
+								//Wrong file type inform the user
+								System.out.println("Wrong File type! Please select a .java, .c, .cpp, or .cs file");
 							}
-						}
-						
-						//Lastly print the line back to our output
-						writer.write(newLine);
-						writer.write(System.getProperty("line.separator"));
+							else validFile = true;
 					}
-					
-					//Output all of the reserved words we found
-					//Output the format
-					System.out.println("Capitalized words (Word, Number of occurences)");
-					
-					//Also create some counters
-					int numWords = 0;
-					long numOccurences = 0;
-					for(String key : reserved.keySet()) { 
-						
-						//Output the key and the value if the value is greater than zero
-						if(reserved.get(key) > 0) {
-							
-							//Print out the word
-							System.out.println(key + ":" + reserved.get(key));
-							
-							//Increase our counters
-							numWords++;
-							numOccurences = numOccurences + reserved.get(key);
-						}
+					else {
+
+						//They pressed cancel
+						System.out.println("Thank you for using the Capitalizer Application!");
+						//Exit the program
+						System.exit(0);
+
 					}
-					
-					//Output our number of words and occurences
-					System.out.println("Total type of words found: " + Integer.toString(numWords));
-					System.out.println("Total number of words replaced: " + Long.toString(numOccurences));
-					
-					//Exit
-					scan.close();
-					writer.close();
-					System.out.println("Output file created at: " + outputFile.getAbsolutePath());
-					System.out.println("Thank you for using the Capitalizer Application!");
-					
-				} catch (IOException e) {
-					
-					//scanner failed
-					System.out.println("There was an error reading/writing the file...Thank you for using the Capitalizer Application!");
+			}
+
+			//Call the function to read/capitalize/and write the file
+			capitalizeSource(selectedFile);
+
+			//Thank the user
+			System.out.println("Thank you for using the Capitalizer Application!");
+	}
+
+	private static void capitalizeSource(File selectedFile) {
+
+		//They entered the correct file type
+		//create a map to assign our array to
+		HashMap<String, Integer> reserved = new HashMap<String, Integer>();
+
+		//Find what file type it was and use the correct array to assign the keys of our map
+		if(selectedFile.getName().endsWith(".java")) reserved = setKeys(reserved, javaWords);
+		else if(selectedFile.getName().endsWith(".c")) reserved = setKeys(reserved, cWords);
+		else if(selectedFile.getName().endsWith(".cpp")) reserved = setKeys(reserved, cppWords);
+		else if(selectedFile.getName().endsWith(".cs")) reserved = setKeys(reserved, csWords);
+
+		try {
+
+			//Create a scanner and read in every line and capitalize the reserved words
+			Scanner scan = new Scanner(selectedFile);
+
+			//Create our FileWriter and print to another file
+			//First create the File, using the directory the user launched the app
+			File outputFile = new File(System.getProperty("user.dir") + "/capitalized.txt");
+			//Delete the file so we can re-write it
+			outputFile.delete();
+			FileWriter writer = new FileWriter(outputFile, true);
+
+			//loop until no more lines
+			while(scan.hasNextLine()) {
+
+				//read in the line
+				String newLine = scan.nextLine();
+
+				//Procedd only if it is not commented
+				if(!newLine.contains("//")) {
+
+					//find if the line has any reserved words
+					for(String key : reserved.keySet()) {
+
+							//Count the number of times it occurs
+							Pattern regex = Pattern.compile(key + "[//{//( ]|" + key + "$");
+								Matcher m = regex.matcher(newLine);
+								int count = 0;
+								while (m.find()){
+
+								//capitalize the sub string
+								newLine = newLine.replaceFirst(key, key.toUpperCase());
+
+									count++;
+								}
+
+							//Increase the value of the key at that point
+							reserved.put(key, reserved.get(key) + count);
+					}
+				}
+
+				//Lastly print the line back to our output
+				writer.write(newLine);
+				writer.write(System.getProperty("line.separator"));
+			}
+
+			//Output all of the reserved words we found
+			//Output the format
+			System.out.println("Capitalized words (Word, Number of occurences)");
+
+			//Also create some counters
+			int numWords = 0;
+			long numOccurences = 0;
+			for(String key : reserved.keySet()) {
+
+				//Output the key and the value if the value is greater than zero
+				if(reserved.get(key) > 0) {
+
+					//Print out the word
+					System.out.println(key + ":" + reserved.get(key));
+
+					//Increase our counters
+					numWords++;
+					numOccurences = numOccurences + reserved.get(key);
 				}
 			}
-		}
-		else {
-			//The pressed cancel
-			System.out.println("Thank you for using the Capitalizer Application!");
-		}
 
+			//Output our number of words and occurences
+			System.out.println("Total type of words found: " + Integer.toString(numWords));
+			System.out.println("Total number of words replaced: " + Long.toString(numOccurences));
+
+			//Exit
+			scan.close();
+			writer.close();
+			System.out.println("Output file created at: " + outputFile.getAbsolutePath());
+
+		} catch (IOException e) {
+
+			//scanner failed
+			System.out.println("There was an error reading/writing the file...Thank you for using the Capitalizer Application!");
+		}
 	}
-	
-	
+
+
 	private static HashMap<String, Integer> setKeys(HashMap<String, Integer> map, String[] array) {
-		
+
 		//Simply loop through the array and add the value to the map
 		for(int i = 0; i < array.length; ++i) {
-			
+
 			//Add it to the map, and init at zero
 			map.put(array[i], 0);
 		}
-		
+
 		//return the map
 		return map;
 	}
