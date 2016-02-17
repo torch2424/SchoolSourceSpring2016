2706eb5c5229d232d64b3e6818c4227c879c718a
Aaron Turner <aaron@aaronthedev.com>
2016-02-16 20:52:34 -0800

    Finished the lab
    Seems to be a much easier lab, and got it working with the example the
    professor specified as an example question asked for demoing

--- OsLab3/lab3.c
+++ OsLab3/lab3.c
@@ -1,7 +1,8 @@
 /**************************************************************************/
 /* PROGRAM: lab3.c */
 /* DESCRIPTION: This program uses exec to call */
-/* various user given commands */
+/* various user given commands, and the command's */
+/* parameters */
 /**************************************************************************/
 # include <stdio.h>
 # include <stdlib.h>
@@ -10,12 +11,21 @@
 # include <unistd.h>
 int main(int argc, char *argv[])
 {
+
+    //Check if the user used the correct input
      if (argc <= 1) {
 
-         printf("\n Usage: %s text_file\n", *argv);
+         //Print the usage and exit
+         //If wront input
+         printf("\nUsage:\n");
+         printf("\n %s [Program To Run] [Program's Parameters]\n", argv[0]);
          exit(1);
      }
 
+     //Run the program and it's parameters
+     //&argv is a two-dimensional array that points
+     //to all of the arguments that are passed into the
+     //Program we are calling
      execvp(argv[1], &argv[1]);
      perror ("exec failure");
      exit(1);
