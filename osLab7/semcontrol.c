31cb5b57978eba5627b16576a0e01702ab036d77
Aaron Turner <aaron@aaronthedev.com>
2016-04-05 21:31:59 -0700

    Finsihed the project Was easy, simply was a obseve lab

--- osLab7/semcontrol.c
+++ osLab7/semcontrol.c
@@ -20,25 +20,36 @@
 # include <time.h>
 # define NS 3
 
+//Create our union for our semaphores
 union semun {
     int val; struct semid_ds *buf; ushort *array;
 };
 
 int main(void)
 {
+
+    //Initialize our variables
     int sem_id, sem_value, i; key_t ipc_key; struct semid_ds sem_buf;
     static ushort sem_array[NS] = {3, 1, 4}; union semun arg;
+
+    //get our key to generate our semphore id's
     ipc_key = ftok(".", 'S');
+
     /* Create semaphore */
-    if ((sem_id = semget(ipc_key, NS, IPC_CREAT | 0666)) == -1) {
+    //IPC_CREAT in part A and B, IPC_CREAT | IPC_EXCL in part c
+    if ((sem_id = semget(ipc_key, NS, IPC_CREAT | IPC_EXCL | 0666)) == -1) {
 
         perror ("semget: IPC | 0666");
         exit(1);
     }
 
+    //Show the semphore Identifier
     printf ("Semaphore identifier %d\n", sem_id);
+
+
     /* Set arg (the union) to the address of the storage location for */
     /* returned semid_ds value */
+    //Return the semphores values
     arg.buf = &sem_buf;
     if (semctl(sem_id, 0, IPC_STAT, arg) == -1) {
 
@@ -47,13 +58,16 @@ int main(void)
     }
 
     printf ("Create %s", ctime(&sem_buf.sem_ctime));
+
     /* Set arg (the union) to the address of the initializing vector */
+    // Set all of the semaphore's values to arg
     arg.array = sem_array;
     if (semctl(sem_id, 0, SETALL, arg) == -1) {
         perror("semctl: SETALL");
         exit(3);
     }
 
+    //Loop through our semaphores and print their values
     for (i=0; i<NS; ++i) {
         if ((sem_value = semctl(sem_id, i, GETVAL, 0)) == -1) {
 
@@ -65,8 +79,9 @@ int main(void)
     }
 
     /* remove semaphore */
-    if (semctl(sem_id, 0, IPC_RMID, 0) == -1) {
-        perror ("semctl: IPC_RMID");
-        exit(5);
-    }
+    //Removed in part B and C of the lab
+    // if (semctl(sem_id, 0, IPC_RMID, 0) == -1) {
+    //     perror ("semctl: IPC_RMID");
+    //     exit(5);
+    // }
 }
